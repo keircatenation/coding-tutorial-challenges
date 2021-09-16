8c4763829901dea32b53fdecff129278e92af7c4
Keiran Pillman <keiran.pillman@gmail.com>
2021-09-16 17:57:54 -0400

    Hooked up Storybook into the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from './stories/Button/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,33 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Button primary={true} backgroundColor={"#000"} size={"large"} label={`count is ${count}`} onClick={() => setCount((count) => count + 1)}/>
       </header>
     </div>
   )
