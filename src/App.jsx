82fd4ac94c8d7014187491320d2c4642241d1177
Keiran Pillman <keiran.pillman@gmail.com>
2021-09-22 13:48:34 -0400

    changed up some stuff with file folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { Button } from './stories/Button/Button'
+import Todo from './stories/components/Todo/todo'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <Button primary={true} backgroundColor={"#000"} size={"large"} label={`count is ${count}`} onClick={() => setCount((count) => count + 1)}/>
-      </header>
+      
+      <Todo/>
     </div>
   )
 }
